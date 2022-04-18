44dd1332556221e669bba016a2e0d63cf0665265
Mehdi Chinoune <mehdi.chinoune@hotmail.com>
2022-04-18 05:54:31 +0100

    Findgl2ps: Detect version

--- cmake/Findgl2ps.cmake
+++ cmake/Findgl2ps.cmake
@@ -7,12 +7,29 @@ find_path(GL2PS_INCLUDE_DIR NAMES gl2ps.h)
 
 find_library(GL2PS_LIBRARY NAMES gl2ps)
 
+set ( _VERSION_FILE ${GL2PS_INCLUDE_DIR}/gl2ps.h )
+if ( EXISTS ${_VERSION_FILE} )
+  foreach(_ver MAJOR MINOR PATCH EXTRA)
+    file ( STRINGS ${_VERSION_FILE} _VERSION_LINE REGEX "define[ ]+GL2PS_${_ver}_VERSION" )
+    if ( _VERSION_LINE )
+      string ( REGEX REPLACE ".*define[ ]+GL2PS_${_ver}_VERSION[ ]+\"?([^ \"]*)\"?.*" "\\1" GL2PS_${_ver}_VERSION "${_VERSION_LINE}" )
+    endif ()
+  endforeach()
+  set(GL2PS_VERSION_STRING "${GL2PS_MAJOR_VERSION}.${GL2PS_MINOR_VERSION}.${GL2PS_PATCH_VERSION}")
+  if(GL2PS_EXTRA_VERSION)
+    set(GL2PS_VERSION_STRING "${GL2PS_VERSION_STRING}${GL2PS_EXTRA_VERSION}")
+  endif()
+endif ()
+unset ( _VERSION_FILE )
+
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args( gl2ps
   FOUND_VAR GL2PS_FOUND
   REQUIRED_VARS
     GL2PS_LIBRARY
     GL2PS_INCLUDE_DIR
+  VERSION_VAR
+    GL2PS_VERSION_STRING
 )
 
 if(GL2PS_FOUND)
